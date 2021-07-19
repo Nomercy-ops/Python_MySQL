ddf0c9d1c0f0a3e8c07e5f65839069031bcb639e
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-07-19 22:01:31 +0530

    [Rikesh]Add: main file for running the crud operations

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+@Author: Rikesh Chhetri
+@Date: 2021-07-19
+@Last Modified by: Rikesh Chhetri
+@Last Modified time: 2021-07-19 10:03:30
+@Title : Program Aim perform CRUD operations and storing data in mysql database. 
+"""
+
+from src.crud_operations import CrudOperations
+from LoggerHandler import logger
+
+def menu():
+    """
+    Description:
+        This function is used for calling crud operations methods.
+        Table name is passed to the init method of crud operation class.
+       
+    """
+    try:
+        print("welcome to crud operations")
+        crud = CrudOperations()
+    
+    except Exception as e:
+        logger.error(e)
+
+if __name__ == "__main__":
+    menu()
