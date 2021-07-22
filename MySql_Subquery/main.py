fe90c10ed2873a000c0fdba4ce90366ce507af74
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-07-22 22:22:31 +0530

    [Rikesh]Add: main file for running mysql subquery programs

---
+++ MySql_Subquery/main.py
@@ -0,0 +1,25 @@
+"""
+@Author: Rikesh Chhetri
+@Date: 2021-07-22
+@Last Modified by: Rikesh Chhetri
+@Last Modified time: 2021-07-22 10:03:30
+@Title : Program Aim is to use mysql subquery.
+"""
+from MySql_Subquery.subquery import Subquery
+from LoggerHandler import logger
+
+def menu():
+    """
+    Description:
+        This function is used for calling subquery class methods. 
+       
+    """
+    try:
+        subquery = Subquery()
+        subquery.in_and_where()
+      
+    except Exception as e:
+        logger.error(e)
+
+if __name__ == "__main__":
+    menu()
